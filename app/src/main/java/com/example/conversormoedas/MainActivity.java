c9dab05bd539f928c4b5193d65d7bc56773fe01d
juliaathar <juathar@gmail.com>
2024-01-11 21:41:36 -0300

    feito parte lógica do aplicativo

--- app/src/main/java/com/example/conversormoedas/MainActivity.java
+++ app/src/main/java/com/example/conversormoedas/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.conversormoedas;
 
 import android.os.Bundle;
+import android.widget.*;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -16,6 +17,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView resultado;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -26,5 +28,41 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //start
+
+        resultado = findViewById(R.id.txt_resultado);
+
+        Button botaoConverter = findViewById(R.id.btn_converter);
+
+        botaoConverter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                converter();
+            }
+
+            private void converter() {
+                RadioGroup opcoes = findViewById(R.id.options);
+                int selecionada = opcoes.getCheckedRadioButtonId();
+
+                EditText valor = findViewById(R.id.input_valor);
+
+                double valorConvertido = 0.0;
+
+                if (selecionada == R.id.radio_usd){
+                    double valorCalculado = Double.parseDouble(valor.getText().toString());
+                    valorConvertido = valorCalculado * 4.87;
+                }else if(selecionada == R.id.radio_eur){
+                    double valorCalculado = Double.parseDouble(valor.getText().toString());
+                    valorConvertido = valorCalculado * 5.35;
+                }else {
+                    double valorCalculado = Double.parseDouble(valor.getText().toString());
+                    valorConvertido = valorCalculado * 0.0054;
+                }
+
+                resultado.setText(String.valueOf(valorConvertido));
+
+                resultado.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
